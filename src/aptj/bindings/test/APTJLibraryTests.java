377b1d93eceb5ddfc2643573795b80b6d54b4194
Loic Alain Royer <royerloic@gmail.com>
2016-09-03 16:29:02 +0200

    First commit: jnaerator worked and generates reasonable code, tests needed...

---
+++ src/aptj/bindings/test/APTJLibraryTests.java
@@ -0,0 +1,16 @@
+package aptj.bindings.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class APTJLibraryTests
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
