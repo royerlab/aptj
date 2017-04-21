8ccbf941f471889fc269fa08cc617de47591c11e
Loic Royer <royer@mpi-cbg.de>
2017-04-21 14:54:20 +0200

    minor

--- src/aptj/test/APTJTests.java
+++ src/aptj/test/APTJTests.java
@@ -8,8 +8,8 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 import aptj.APTJDevice;
-import aptj.APTJDeviceType;
 import aptj.APTJDeviceFactory;
+import aptj.APTJDeviceType;
 
 /**
  * APTJ Tests
@@ -19,68 +19,73 @@ import aptj.APTJDeviceFactory;
 public class APTJTests
 {
 
-	/**
-	 * Tests TST001 device
-	 * @throws Exception NA
-	 */
-	@Test
-	public void testTST001Device() throws Exception
-	{
-		try (APTJDeviceFactory lAPTJLibrary = new APTJDeviceFactory(APTJDeviceType.TST001))
-		{
+  /**
+   * Tests TST001 device
+   * 
+   * @throws Exception
+   *           NA
+   */
+  @Test
+  public void testTST001Device() throws Exception
+  {
+    try (
+        APTJDeviceFactory lAPTJLibrary =
+                                       new APTJDeviceFactory(APTJDeviceType.TST001))
+    {
 
-			int lNumberOfDevices = lAPTJLibrary.getNumberOfDevices();
+      int lNumberOfDevices = lAPTJLibrary.getNumberOfDevices();
 
-			System.out.println(lNumberOfDevices);
+      System.out.println(lNumberOfDevices);
 
-			APTJDevice lDevice = lAPTJLibrary.createDeviceFromIndex(0);
+      APTJDevice lDevice = lAPTJLibrary.createDeviceFromIndex(0);
 
-			System.out.println("home()");
-			lDevice.home();
-			assertTrue(lDevice.waitWhileMoving(	10,
-																					10 * 1000,
-																					TimeUnit.MILLISECONDS));
-			assertEquals(lDevice.getCurrentPosition(), 0, 0.01);
+      System.out.println("home()");
+      lDevice.home();
+      assertTrue(lDevice.waitWhileMoving(10,
+                                         30 * 1000,
+                                         TimeUnit.MILLISECONDS));
+      assertEquals(lDevice.getCurrentPosition(), 0, 0.01);
 
-			System.out.println("moveTo(2)");
-			lDevice.moveTo(2);
-			assertTrue(lDevice.waitWhileMoving(	10,
-																					10 * 1000,
-																					TimeUnit.MILLISECONDS));
-			assertEquals(lDevice.getCurrentPosition(), 2, 0.01);
+      System.out.println("moveTo(2)");
+      lDevice.moveTo(2);
+      assertTrue(lDevice.waitWhileMoving(10,
+                                         10 * 1000,
+                                         TimeUnit.MILLISECONDS));
+      assertEquals(lDevice.getCurrentPosition(), 2, 0.01);
 
-			System.out.println("moveBy(-1)");
-			lDevice.moveBy(-1);
-			assertTrue(lDevice.waitWhileMoving(	10,
-																					10 * 1000,
-																					TimeUnit.MILLISECONDS));
-			assertEquals(lDevice.getCurrentPosition(), 1, 0.01);
+      System.out.println("moveBy(-1)");
+      lDevice.moveBy(-1);
+      assertTrue(lDevice.waitWhileMoving(10,
+                                         10 * 1000,
+                                         TimeUnit.MILLISECONDS));
+      assertEquals(lDevice.getCurrentPosition(), 1, 0.01);
 
-			System.out.println("move(1)");
-			lDevice.move(1);
-			Thread.sleep(20000);
-			assertTrue(lDevice.getCurrentPosition() > lDevice.getMaxPosition() / 2);
+      System.out.println("move(1)");
+      lDevice.move(1);
+      Thread.sleep(20000);
+      assertTrue(lDevice.getCurrentPosition() > lDevice.getMaxPosition()
+                                                / 2);
 
-			System.out.println("moveTo(1)");
-			lDevice.moveTo(1);
-			assertTrue(lDevice.waitWhileMoving(	10,
-																					10 * 1000,
-																					TimeUnit.MILLISECONDS));
-			assertEquals(lDevice.getCurrentPosition(), 1, 0.01);
+      System.out.println("moveTo(1)");
+      lDevice.moveTo(1);
+      assertTrue(lDevice.waitWhileMoving(10,
+                                         10 * 1000,
+                                         TimeUnit.MILLISECONDS));
+      assertEquals(lDevice.getCurrentPosition(), 1, 0.01);
 
-			System.out.println("move(-1)");
-			lDevice.move(-1);
-			Thread.sleep(3000);
-			assertTrue(lDevice.getCurrentPosition() < 1);
+      System.out.println("move(-1)");
+      lDevice.move(-1);
+      Thread.sleep(3000);
+      assertTrue(lDevice.getCurrentPosition() < 1);
 
-			System.out.println("home()");
-			lDevice.home();
-			assertTrue(lDevice.waitWhileMoving(	10,
-																					10 * 1000,
-																					TimeUnit.MILLISECONDS));
-			assertEquals(lDevice.getCurrentPosition(), 0, 0.5);
+      System.out.println("home()");
+      lDevice.home();
+      assertTrue(lDevice.waitWhileMoving(10,
+                                         10 * 1000,
+                                         TimeUnit.MILLISECONDS));
+      assertEquals(lDevice.getCurrentPosition(), 0, 0.5);
 
-		}
-	}
+    }
+  }
 
 }
